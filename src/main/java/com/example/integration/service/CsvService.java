8f7aef38465aab4639db9bcadb8f678bb112fed3
Pedro Gabriel <pedro.correia@germinare.org.br>
2025-09-14 10:39:02 -0300

    chore: Update on reading CSV file

--- src/main/java/com/example/integration/service/CsvService.java
+++ src/main/java/com/example/integration/service/CsvService.java
@@ -8,9 +8,11 @@ import com.opencsv.CSVReaderBuilder;
 import com.opencsv.exceptions.CsvValidationException;
 import org.springframework.stereotype.Service;
 
-import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Path;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,29 +21,35 @@ public class CsvService {
 
     public CsvService() {}
 
-    public List<LeiDto> readCsv(Path filePath) throws CsvValidationException, IOException {
+    public List<LeiDto> readCsv(InputStream inputStream) throws CsvValidationException, IOException {
         List<LeiDto> leis = new ArrayList<>();
         CSVParser parser = new CSVParserBuilder().withSeparator(';').build();
-        CSVReader reader = new CSVReaderBuilder(new FileReader(filePath.toFile()))
-                .withCSVParser(parser)
-                .build();
-        String[] line;
-        reader.readNext();
-        while ((line = reader.readNext()) != null){
-            if (!line[5].equals("EXTERNO")) {
-                leis.add(new LeiDto(
-                        Integer.parseInt(line[0]),
-                        line[1],
-                        line[2],
-                        line[3],
-                        line[4],
-                        line[5],
-                        line[6],
-                        line[7]
-                ));
+
+        try (Reader reader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+             CSVReader csvReader = new CSVReaderBuilder(reader)
+                     .withCSVParser(parser)
+                     .build()) {
+
+            String[] line;
+            csvReader.readNext(); // pula cabeçalho
+
+            while ((line = csvReader.readNext()) != null) {
+                if (!line[5].equals("EXTERNO")) {
+                    leis.add(new LeiDto(
+                            Integer.parseInt(line[0]),
+                            line[1],
+                            line[2],
+                            line[3],
+                            line[4],
+                            line[5],
+                            line[6],
+                            line[7]
+                    ));
+                }
             }
         }
 
         return leis;
     }
+
 }
