100e91a0996045cca6b7d1eb2c51628d267a8592
Pedro Gabriel <pedro.correia@germinare.org.br>
2025-10-11 09:21:32 -0300

    fix: fix object serialization in @Cacheable

--- src/main/java/com/example/integration/service/CnpjService.java
+++ src/main/java/com/example/integration/service/CnpjService.java
@@ -3,6 +3,7 @@ package com.example.integration.service;
 import com.example.integration.client.cnpj.RobustCnpjClient;
 import com.example.integration.client.cnpj.response.WsData;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,6 +12,7 @@ public class CnpjService {
     @Autowired
     private RobustCnpjClient robustCnpjClient;
 
+    @Cacheable("enterprise")
     public WsData getEnterpriseByCnpj(String cnpj){
         return robustCnpjClient.getEnterpriseByCnpj(sanitizeCnpj(cnpj));
     }
